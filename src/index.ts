fdfaa8f2188774fe356bff6a2f325895bbe71c5a
Dean <o.michaeldean@gmail.com>
2024-08-23 03:29:05 +0100

    project init

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { createApp } from "@deroll/app";
+import { createWallet } from "@deroll/wallet";
+import { getAddress, hexToString, stringToHex } from "viem";
+
+const app = createApp({url: process.env.ROLLUP_HTTP_SERVER_URL || "http://127.0.0.1:5004"})
+
+const wallet = createWallet();
+app.addAdvanceHandler(wallet.handler);
+
+app.addAdvanceHandler( async ({ metadata, payload}) => {
+  console.log("Iput: ", metadata, payload);
+  const sender = getAddress(metadata.msg_sender);
+
+  try {
+    const jsonPayload = JSON.parse(hexToString(payload));
+
+    if (jsonPayload.method === "ether_withdraw") {
+      console.log("Withdrawing ether");
+
+      const amountToWithdraw: bigint = BigInt(jsonPayload.amount);
+      const voucher = wallet.withdrawEther(sender, amountToWithdraw);
+
+      await app.createVoucher(voucher);
+      return "accept";
+    }
+  } catch(e) {
+    app.createReport({payload: stringToHex(String(e))});
+    return "reject";
+  }
+  return "accept";
+});
+
+app.addInspectHandler(async ({payload}) => {
+  const url = hexToString(payload).split("/");
+  console.log("Inspect call:", url);
+  const eth_balance = wallet.etherBalanceOf(<string>url[1])
+
+
+  await app.createReport( {payload: stringToHex(String(eth_balance))})
+})
+
+
+app.start().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
